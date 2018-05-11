93551b067a32e77b1221af0c3e04f7b8b1c75594
Nick <a.grinner@gmail.com>
2018-05-11 16:19:43 -0400

    version bump

--- libnano/__init__.py
+++ libnano/__init__.py
@@ -8,7 +8,7 @@
 __author__: str = "Nick Conway, Ben Pruitt"
 __copyright__ = 'Copyright 2018, Nick Conway; Wyss Institute Harvard University'
 __license__ = 'GPL2'
-__version__: str = '0.2.2.0'
+__version__: str = '0.2.2.1'
 
 import os
 from typing import List
